c63bbd2559bfb5ba1734168fbba4536664bd2bf2
Pearsteam <93873756+Birnendampf@users.noreply.github.com>
2026-02-17 15:18:54 +0100

    copy over nbtcompare

--- NBTCompare/src/nbtcompare/__init__.py
+++ NBTCompare/src/nbtcompare/__init__.py
@@ -1,5 +1,6 @@
-from nbtcompare._core import hello_from_bin
+"""A simple package for comparing NBT Compound Tags.
 
+A pure python reference implementation of this package is available at https://github.com/Birnendampf/MineDelta.
+"""
 
-def hello() -> str:
-    return hello_from_bin()
+from nbtcompare._core import compare as compare
