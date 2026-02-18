e8a7af0b38c47bc0cde3440b76ad68d4cd11d63e
Pearsteam <93873756+Birnendampf@users.noreply.github.com>
2026-02-18 23:15:10 +0100

    add missing annotations

--- tests/test__dummy_executor.py
+++ tests/test__dummy_executor.py
@@ -1,10 +1,12 @@
+from typing import NoReturn
+
 import pytest
 
 # noinspection PyProtectedMember
 from minedelta import _dummy_executor
 
 
-def some_func(arg: int, /, *, kwarg: int):
+def some_func(arg: int, /, *, kwarg: int) -> int:
     return arg + kwarg
 
 
@@ -12,18 +14,18 @@ class SomeError(Exception):
     pass
 
 
-def raises():
+def raises() -> NoReturn:
     raise SomeError()
 
 
 class TestDummyExecutor:
-    def test_submit(self):
+    def test_submit(self) -> None:
         with _dummy_executor.DummyExecutor() as executor:
             submit = executor.submit(some_func, 1, kwarg=2)
             assert submit.result() == 3
             with pytest.raises(SomeError):
                 executor.submit(raises)
 
-    def test_map(self):
+    def test_map(self) -> None:
         with _dummy_executor.DummyExecutor() as executor:
             assert tuple(executor.map(sum, zip(range(3), range(3), strict=True))) == (0, 2, 4)
