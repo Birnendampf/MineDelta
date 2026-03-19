974b7c42135104b78a0ef25e14c022dd88a0e374
Pearsteam <93873756+Birnendampf@users.noreply.github.com>
2026-03-19 19:33:28 +0100

    add arguments to benchmark all managers

--- benchmark/bench.py
+++ benchmark/bench.py
@@ -2,7 +2,6 @@
 # requires-python = ">=3.11"
 # dependencies = [
 #     "minedelta[standard]",
-#     "platformdirs>=4.9.4",
 # ]
 #
 # [tool.uv.sources]
@@ -17,6 +16,8 @@ import shutil
 import sys
 from pathlib import Path
 from typing import TYPE_CHECKING
+
+from minedelta import backup
 
 # noinspection PyProtectedMember
 from minedelta._thread_scope import MAX_WORKERS
@@ -49,17 +50,84 @@ def main() -> None:  # noqa: D103
     verbosity_group.add_argument("-q", "--quiet", action="store_true")
     verbosity_group.add_argument("-v", "--verbose", action="count", default=0)
 
-    sub = parser.add_subparsers(title="Commands", required=True)
+    sub = parser.add_subparsers(title="commands", required=True)
 
     capture_help = "Capture a snapshot of the world"
     capture = sub.add_parser("capture", help=capture_help, description=capture_help)
+    capture.set_defaults(func=_capture, manager=None)
     capture.add_argument("--clean", action="store_true", help="Remove all existing snapshots")
     capture.add_argument("world", type=Path, help="Path of the world to capture")
-    capture.set_defaults(func=_capture)
 
+    run_help = "Run a benchmark"
+    run = sub.add_parser("run", help=run_help, description=run_help)
+    run.set_defaults(func=_run)
+
+    action_group = run.add_argument_group(
+        "actions", "which actions to benchmark. Performs all by default."
+    )
+    for action in ("create", "restore", "delete"):
+        action_group.add_argument(
+            f"-{action[:1]}",
+            f"--{action}",
+            action="append_const",
+            const=action,
+            dest="action",
+            help=f"benchmark {action[:-1]}ing backups",
+        )
+
+    manager_group = run.add_argument_group(
+        "managers", "which manager to benchmark. Defaults to all if not specified."
+    )
+    for manager in (
+        backup.HardlinkBackupManager,
+        backup.GitBackupManager,
+        backup.DiffBackupManager,
+    ):
+        manager_group.add_argument(
+            f"--{manager.__name__[:-13].lower()}",
+            action="append_const",
+            const=manager,
+            dest="manager",
+            help=((manager.__doc__ or "").splitlines()[0]),
+        )
     args = parser.parse_args()
     _set_verbosity(args)
     args.func(args)
+
+
+def _capture(args: argparse.Namespace) -> None:
+    world: Path = args.world.expanduser()
+    capture_directory: Path = args.capture_directory.expanduser()
+    existing_count = 0
+    if capture_directory.is_dir():
+        existing_count = len(list(capture_directory.iterdir()))
+        logger.debug(f"Found {existing_count} existing snapshots")
+    if args.clean:
+        logger.info("Cleaning up old snapshots")
+        shutil.rmtree(capture_directory, onerror=_rmtree_on_error)
+        existing_count = 0
+    capture_directory.mkdir(parents=True, exist_ok=True)
+    new_capture = capture_directory / f"{existing_count}.bak"
+    # noinspection PyProtectedMember
+    with tarfile.open(  # type: ignore[call-overload]
+        new_capture,
+        "x:" + diff._DEFAULT_COMPRESSION,
+        options={400: MAX_WORKERS},
+        debug=max(args.verbose - 1, 0),
+    ) as tar:
+        _patch_tar_logger(tar, logger)
+        logger.info(f"Compressing {world}...")
+        tar.add(world, ".")
+    logger.debug(f"Finished ({new_capture.stat().st_size // 2**20}MiB)")
+
+
+def _run(args: argparse.Namespace) -> None:
+    actions: set[str] = set(args.action or ("create", "restore", "delete"))
+    managers = set(
+        args.manager
+        or (backup.HardlinkBackupManager, backup.GitBackupManager, backup.DiffBackupManager)
+    )
+    print(args, f"{actions=}, {managers=}")
 
 
 def _set_verbosity(args: argparse.Namespace) -> None:
@@ -88,32 +156,6 @@ def _rmtree_on_error(
     exc_info: tuple[type[BaseException], BaseException, "TracebackType"],
 ) -> None:
     logger.warning(f"failed to remove {path}", exc_info=exc_info)
-
-
-def _capture(args: argparse.Namespace) -> None:
-    world: Path = args.world.expanduser()
-    capture_directory: Path = args.capture_directory.expanduser()
-    existing_count = 0
-    if capture_directory.is_dir():
-        existing_count = len(list(capture_directory.iterdir()))
-        logger.debug(f"Found {existing_count} existing snapshots")
-    if args.clean:
-        logger.info("Cleaning up old snapshots")
-        shutil.rmtree(capture_directory, onerror=_rmtree_on_error)
-        existing_count = 0
-    capture_directory.mkdir(parents=True, exist_ok=True)
-    new_capture = capture_directory / f"{existing_count}.bak"
-    # noinspection PyProtectedMember
-    with tarfile.open(  # type: ignore[call-overload]
-        new_capture,
-        "x:" + diff._DEFAULT_COMPRESSION,
-        options={400: MAX_WORKERS},
-        debug=max(args.verbose - 1, 0),
-    ) as tar:
-        _patch_tar_logger(tar, logger)
-        logger.info(f"Compressing {world}...")
-        tar.add(world, ".")
-    logger.debug(f"Finished ({new_capture.stat().st_size // 2**20}MiB)")
 
 
 if __name__ == "__main__":
